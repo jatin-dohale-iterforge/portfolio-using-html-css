a91e754fc18eac84db7a356595c2d3023dea971a
jatin-dohale-iterforge <jatin.dohale@iterforge.com>
2026-07-03 18:57:13 +0530

    Added Name,Number and Empty Validation

--- registration.js
+++ registration.js
@@ -1,3 +1,36 @@
+// function for name validation
+const nameChecker = (input, str) => {
+    const isName = /^[A-Za-z]+$/.test(str)
+    if(str.length == 0 ) return
+    if (!isName) {
+        input.parentElement.nextElementSibling.innerText = "Please Enter Valid Name";
+        input.parentElement.nextElementSibling.classList.remove("invisible");
+    }else{
+        input.parentElement.nextElementSibling.classList.add("invisible");
+    }
+
+}
+
+const firstName = document.getElementById("first-name")
+firstName.addEventListener("input",(e)=>nameChecker(e.target,e.target.value))
+
+const middleName = document.getElementById("middle-name")
+middleName.addEventListener("input",(e)=>nameChecker(e.target,e.target.value))
+
+const lastName = document.getElementById("last-name")
+lastName.addEventListener("input",(e)=>nameChecker(e.target,e.target.value))
+
+
+// Number Checker
+const numberChecker = (numberInput) =>{
+    if(numberInput.value.length == 0 ) return
+     if (!(numberInput.value.length == 10)) {
+        numberInput.parentElement.nextElementSibling.innerText = "Please Enter Valid Number";
+        numberInput.parentElement.nextElementSibling.classList.remove("invisible");
+    }
+}
+
+
 
 //  Creating floating label for name
 const box = document.querySelectorAll("#student-name-box > div > div");
@@ -6,7 +39,7 @@ box.forEach((inputBox) => {
     inputBox.addEventListener("click", () => {
         inputBox.classList.add("active-box")
         inputBox.children[0].classList.add("active-text")
-        inputBox.children[1].className ="active-input";
+        inputBox.children[1].className = "active-input";
         inputBox.children[1].focus()
     })
 })
@@ -19,7 +52,7 @@ box2.forEach((inputBox) => {
     inputBox.addEventListener("click", () => {
         inputBox.classList.add("active-box")
         inputBox.children[0].classList.add("active-text")
-        inputBox.children[1].className ="active-input";
+        inputBox.children[1].className = "active-input";
         inputBox.children[1].focus()
     })
 })
@@ -39,38 +72,55 @@ aboutCheck.addEventListener("click", () => {
 
 // Form Validation
 const form = document.querySelector("form");
-const formInputs  = document.querySelectorAll("form input");
-form.addEventListener("submit",(e)=>{
+const formInputs = [...document.querySelectorAll("form input")].slice(0,4);
+const nameInputs = document.querySelectorAll("#student-name-box input")
+const emailNumberInputs = document.querySelectorAll("#email-number-box input")
+
+
+form.addEventListener("submit", (e) => {
     e.preventDefault()
-    formInputs.forEach((input)=>{
-        if(input.value === ""){
+
+    formInputs.forEach((input) => {
+        if (input.value === "") {
             input.parentElement.nextElementSibling.classList.remove("invisible");
-            
         }
     })
+
+    emailNumberInputs.forEach((input)=>{
+        if (input.value === "") {
+            input.parentElement.nextElementSibling.classList.remove("invisible");
+        }
+    })
+    
+    numberChecker(emailNumberInputs[1]);
+
 })
 
 
 // Cancel Button
 const cancelButton = document.getElementById("cancel")
 const genderRadios = document.querySelectorAll("#gender-box input")
-cancelButton.addEventListener("click",(e)=>{
+const errorTexts = [...document.querySelectorAll("form p")].slice(1);
+cancelButton.addEventListener("click", (e) => {
     e.preventDefault();
     box.forEach((inputBox) => {
         inputBox.classList.remove("active-box")
         inputBox.children[0].classList.remove("active-text")
-        inputBox.children[1].className ="deactive-link";
+        inputBox.children[1].className = "deactive-link";
     })
     box2.forEach((inputBox) => {
         inputBox.classList.remove("active-box")
         inputBox.children[0].classList.remove("active-text");
-        inputBox.children[1].className ="deactive-link";
+        inputBox.children[1].className = "deactive-link";
     })
-    formInputs.forEach((input)=>{
+    formInputs.forEach((input) => {
+        console.log(input)
         input.value = ""
     })
-    genderRadios.forEach((radio)=>{
+    genderRadios.forEach((radio) => {
         radio.checked = false
     })
+    errorTexts.forEach((text=>{
+        text.classList.add("invisible");
+    }))
 })
-    
