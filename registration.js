c0565b425d3df81e92d475d8d8e0726f76b4ff44
jatin-dohale-iterforge <jatin.dohale@iterforge.com>
2026-07-03 10:13:42 +0530

    Responsive for Mobile Devices

--- registration.js
+++ registration.js
@@ -1,31 +1,31 @@
 const box = document.querySelectorAll("#student-name-box > div > div");
 
-box.forEach((div) => {
-    div.addEventListener("click", () => {
+box.forEach((inputBox) => {
+    inputBox.addEventListener("click", () => {
         box.forEach((item) => {
             item.classList.remove("active-box")
             item.children[0].classList.remove("active-text")
             item.children[1].classList.remove("active-input")
         })
-        div.classList.add("active-box")
-        div.children[0].classList.add("active-text")
-        div.children[1].classList.add("active-input")
-        div.children[1].focus()
+        inputBox.classList.add("active-box")
+        inputBox.children[0].classList.add("active-text")
+        inputBox.children[1].classList.add("active-input")
+        inputBox.children[1].focus()
     })
 })
 
 const box2 = document.querySelectorAll("#email-number-box > div > div");
 
-box2.forEach((div) => {
-    div.addEventListener("click", () => {
+box2.forEach((inputBox) => {
+    inputBox.addEventListener("click", () => {
         box2.forEach((item) => {
             item.classList.remove("active-box")
             item.children[0].classList.remove("active-text")
             item.children[1].classList.remove("active-input")
         })
-        div.classList.add("active-box")
-        div.children[0].classList.add("active-text")
-        div.children[1].classList.add("active-input")
-        div.children[1].focus()
+        inputBox.classList.add("active-box")
+        inputBox.children[0].classList.add("active-text")
+        inputBox.children[1].classList.add("active-input")
+        inputBox.children[1].focus()
     })
 })
